5f9a546e78306dc58e66fb4f4a52eb5b1992bdf0
ariqserazi <62480217+ariqserazi@users.noreply.github.com>
2022-01-06 01:16:15 -0500

    updated

--- picnic.py
+++ picnic.py
@@ -3,5 +3,17 @@ parser = argparse.ArgumentParser(description= "Picnic" , formatter_class = argpa
 parser.add_argument('item',metavar='str', nargs = "+", help='items to bring')
 parser.add_argument('-s','--sorted',action='store_true', help = "sorts the items")
 arg = parser.parse_args()
-items = arg.items
+items = arg.item
 num = len(items)
+"""if arg.sorted():
+    items.sorted()"""
+bring = ""
+if num == 1:
+    bring = items[0]
+elif num == 2:
+    bring = " and ".join(items)
+else: 
+    items[-1] = "and " + items[-1]
+    bring = ", ".join(items)
+    
+print("you will bring the " + bring)
